fe11c3009454f51a4b95692fb0d52c24cad829be
edsonibarra <edsonibarra97@gmail.com>
2022-11-18 23:55:22 -0600

    base

--- linkedlist.py
+++ linkedlist.py
@@ -1,3 +1,19 @@
+from node import Node
+
+
 class LinkedList:
     def __init__(self):
         self.head = None
+
+    def print_list(self):
+        if self.is_empty():
+            print('Empty List')
+            return
+        print('---PRINTING LIST---')
+        cur_node = self.head
+        while cur_node:
+            print(cur_node.data)
+            cur_node = cur_node.next
+    
+    def is_empty(self):
+        return self.head is None
